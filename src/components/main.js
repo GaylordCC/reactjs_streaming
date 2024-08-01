1751cb66982003904a341b260d82a7a6ce03ed2d
GaylordCC <gcarricaballero3@hotmail.com>
2024-08-01 18:32:42 -0500

    Add conditional for each endpoint name

--- src/components/main.js
+++ src/components/main.js
@@ -4,7 +4,22 @@ const AIJoke = () => {
     const [joke, setJoke] = useState('');
 
     useEffect(() => {
-        const eventSource = new EventSource('http://localhost:8000/main-streaming-endpoint');
+        
+        function getEventSource(endpoint) {
+            switch (endpoint) {
+                case 'main':
+                    return new EventSource('http://localhost:8000/main-streaming-endpoint');
+                case 'chatopenai':
+                    return new EventSource('http://localhost:8000/chatopenai-streaming-endpoint');
+                case 'azureopenai':
+                    return new EventSource('http://localhost:8000/azureopenai-streaming-endpoint');
+                default:
+                    throw new Error('Endpoint no válido');
+            }
+        }
+
+        const endpoint = 'azureopenai';
+        const eventSource = getEventSource(endpoint);
 
         eventSource.onmessage = function(event) {
             setJoke(prevJoke => prevJoke + event.data);
